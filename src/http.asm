04aacba0de9abd5e4b513db1d206d6cbc35fcbda
Joachim-barre <joachim.barre22@laposte.net>
2025-08-14 18:55:42 +0200

    added parsing to the http handler

--- src/http.asm
+++ src/http.asm
@@ -9,6 +9,9 @@ section .bss
 
     http_server equ server
 
+section .data
+    recived_msg: db "recived request: ", 0
+
 section .text
     global http_init
     global http_main_loop
@@ -38,33 +41,81 @@ http_handler: ; (u32 fd)
     push rbp
     mov rbp, rsp
 
-    sub rsp, 16
+    sub rsp, 80
     mov [rbp-8], rdi ; [rbp-8]=fd
     ; [rbp-16]=buf
+    ; [rbp-56]=request
+    ; [rbp-64]=offset
+    ; [rbp-72]=buf_len
 
     mov rdi, 1024
     call malloc
 
     mov [rbp-16], rax
 
+; main loop
+.loop:
     ; read a request
     mov rax, 0 ; sys_read
     mov edi, [rbp-8] ; fd
     mov rsi, [rbp-16] ; buf
-    mov rdx, 1023 ; len=1023 (leave one char to the 0 terminator)
+    mov rdx, 1024 ; len
     syscall
 
     test rax, rax
     js .error
 
-    ; print the request
+    mov [rbp-72], rax ; save buf_len
 
-    ; zero terminate the string
+    ; parse status line
+    
+    ; read the method
+    mov rdi, [rbp-16]
+    mov rsi, rax
+    mov dl, ' '
+    call find_char
+
+    ; put a zero where there was a space
     mov rdi, [rbp-16]
     mov byte [rdi+rax], 0
 
-    ; print the string
+    mov [rbp-56+HttpRequest.method], rdi ; save the method
+    inc rax
+    mov [rbp-64], rax, ; save the offset
+
+    ; read the path
+    mov rdi, [rbp-16]
+    add rdi, [rbp-64]
+    mov rsi, [rbp-72]
+    sub rsi, rax ; substract the offset from the size
+    mov dl, ' '
+    call find_char
+
+    ; put a zero where there was a space
+    mov rdi, [rbp-16]
+    add rdi, [rbp-64]
+    mov byte [rdi+rax], 0
+
+    mov [rbp-56+HttpRequest.path], rdi
+    add [rbp-64], rax, ; save the offset
+
+    ; TODO: parse the rest
+
+    ; log request
+    mov rdi, recived_msg
     call print
+
+    mov rdi, [rbp-56+HttpRequest.method]
+    call print
+
+    mov dil, ' '
+    call putchar
+
+    mov rdi, [rbp-56+HttpRequest.path]
+    call print
+
+    mov dil, 10
+    call putchar
 
     jmp .exit
 
@@ -73,6 +124,10 @@ http_handler: ; (u32 fd)
     call printi
 
 .exit:
+    ; free the buffer
+    mov rdi, [rbp-16]
+    call free
+
     mov rsp, rbp
     pop rbp
 
