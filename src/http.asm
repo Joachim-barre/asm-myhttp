38fad245a04a3534c884795e1bb16c3f3c072677
Joachim-barre <joachim.barre22@laposte.net>
2025-08-14 17:39:48 +0200

    added a basic http handler

--- src/http.asm
+++ src/http.asm
@@ -1,6 +1,8 @@
 %define HTTP_SYMBOLS
 %include "http.inc"
 %include "net.inc"
+%include "mem.inc"
+%include "helpers.inc"
 
 section .bss
     global http_server
@@ -36,8 +38,41 @@ http_handler: ; (u32 fd)
     push rbp
     mov rbp, rsp
 
-    ; TODO 
+    sub rsp, 16
+    mov [rbp-8], rdi ; [rbp-8]=fd
+    ; [rbp-16]=buf
 
+    mov rdi, 1024
+    call malloc
+
+    mov [rbp-16], rax
+
+    ; read a request
+    mov rax, 0 ; sys_read
+    mov edi, [rbp-8] ; fd
+    mov rsi, [rbp-16] ; buf
+    mov rdx, 1023 ; len=1023 (leave one char to the 0 terminator)
+    syscall
+
+    test rax, rax
+    js .error
+
+    ; print the request
+
+    ; zero terminate the string
+    mov rdi, [rbp-16]
+    mov byte [rdi+rax], 0
+
+    ; print the string
+    call print
+
+    jmp .exit
+
+.error:
+    mov rdi, rax
+    call printi
+
+.exit:
     mov rsp, rbp
     pop rbp
 
