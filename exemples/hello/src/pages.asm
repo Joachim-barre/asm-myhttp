b1a0c7d3e82d85f88beadb50c82dc744cae08449
Joachim-barre <joachim.barre22@laposte.net>
2025-08-16 12:40:54 +0200

    added the PAGE_FILE macro

--- exemples/hello/src/pages.asm
+++ exemples/hello/src/pages.asm
@@ -5,11 +5,4 @@ section .data
 
     pages: dq index, 0 ; Page** (page array)
 
-    index: istruc Page
-        at Page.path, dq index_path
-        at Page.kind, dq 0
-        at Page.data0, dq index_data
-    iend
-
-    index_path: db "/", 0
-    index_data: incbin "html/index.html" db 0
+    PAGE_FILE index, "/", "html/index.html" 
