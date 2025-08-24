1140cabfaa9883883f7e2e6252d6cc650a739f4c
Joachim-barre <joachim.barre22@laposte.net>
2025-08-24 18:55:54 +0200

    added index.js

--- exemples/simple_chat/src/pages.asm
+++ exemples/simple_chat/src/pages.asm
@@ -3,7 +3,8 @@
 section .data
     global pages
 
-    pages: dq index, index_css, 0 ; Page** (page array)
+    pages: dq index, index_css, index_js, 0 ; Page** (page array)
 
     PAGE_FILE index, "GET", "/", "html/index.html" 
     PAGE_FILE index_css, "GET", "/static/index.css", "static/index.css"
+    PAGE_FILE index_js, "GET", "/static/index.js", "static/index.js"
