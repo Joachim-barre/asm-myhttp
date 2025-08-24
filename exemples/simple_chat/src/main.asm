ddb1e03ebbc8907e798dde70d5ca9ec1878089bb
Joachim-barre <joachim.barre22@laposte.net>
2025-08-24 18:33:18 +0200

    added initalisation for an app thread in the simple_chat exemple

--- exemples/simple_chat/src/main.asm
+++ exemples/simple_chat/src/main.asm
@@ -1,3 +1,41 @@
 %include "app.inc"
+%include "thread.inc"
+%include "helpers.inc"
+%include "log.inc"
 
-APP_DEFAULT
+section .data
+    global app
+
+    app: istruc App
+        at .init_callback, dq init_callback
+        at .send_wrapper, dq send_with_default_headers
+    iend
+
+section .text
+    required send_with_default_headers
+
+init_callback:
+    push rbp
+    mov rbp, rsp
+
+    info is, {"Initalizing the main app thread", 10}
+
+    mov rdi, app_main
+    mov rsi, 0
+    call thread_init
+
+    mov rsp, rbp
+    pop rbp
+
+    ret
+
+app_main:
+    push rbp
+    mov rbp, rsp
+
+    info is, {"the app's main thread is running", 10}
+
+    mov rsp, rbp
+    pop rbp
+
+    ret
