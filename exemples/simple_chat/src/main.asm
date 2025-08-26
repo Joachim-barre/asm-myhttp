b41e44a179af5d7d7c4af271874dbb009b6260ec
Joachim-barre <joachim.barre22@laposte.net>
2025-08-26 15:52:29 +0200

    added the event callback

--- exemples/simple_chat/src/main.asm
+++ exemples/simple_chat/src/main.asm
@@ -32,6 +32,38 @@ section .data
         at .body, dq 0
     iend
 
+    ok_code equ 200
+    ok_str: db "Ok", 0
+        .len equ $- ok_str-1
+
+    content_type: db "Content-Type", 0
+    event_stream: db "text/event-stream", 0
+
+    LL_STATIC events_headers, HttpHeader.size, events_connection_header, events_content_type
+    LL_STATIC_NODE events_connection_header, , events_content_type, 0
+    istruc HttpHeader
+        at HttpHeader.field, dq connection
+        at HttpHeader.value, dq connection_keep_alive
+    iend
+
+    LL_STATIC_NODE events_content_type, , 0, events_connection_header
+    istruc HttpHeader
+        at HttpHeader.field, dq content_type
+        at HttpHeader.value, dq event_stream
+    iend
+
+    events_responce: istruc HttpResponce
+        at .status_code, dd ok_code
+        at .status_str, dq ok_str
+        at .headers, dq events_headers
+        at .body, dq 0
+    iend
+
+section .bss
+    listeners: resb SpinLock.size ; SpinLock<LinkedList<Fd>>
+
+    listeners_inner: resb LinkedList.size
+
 section .text
     required send_with_default_headers
     extern default_argument_parsing
@@ -59,7 +91,21 @@ app_main:
     push rbp
     mov rbp, rsp
 
+    sub rsp, 48
+    ; [rbp-32] responce
+    ; [rbp-48] body
+
     info is, "the app's main thread is running", c, 10
+
+    lea rdi, [listeners_inner]
+    mov rsi, 4
+    call ll_init
+
+    lea rdi, [listeners]
+    lea rsi, [listeners_inner]
+    call spin_init
+
+.loop:
 
     mov rsp, rbp
     pop rbp
@@ -67,7 +113,40 @@ app_main:
     ret
 
 events_callback: ;  (HttpRequest*, int fd)
-    ret ; TODO
+    push rbp
+    mov rbp, rsp
+
+    sub rsp, 16
+    mov [rbp-8], rdi
+    mov [rbp-12], esi
+
+    info is, "Initalizing SSE connection", c, 10
+
+    mov edi, [rbp-12]
+    lea rsi, [events_responce]
+    call http_send_responce
+
+    ; duplicate the file handle
+    mov rax, 32
+    mov edi, [rbp-12]
+    syscall
+
+    mov [rbp-12], eax
+
+    lea rdi, [listeners]
+    call spin_lock
+
+    mov rdi, rax
+    lea rsi, [rbp-12]
+    call ll_push_back
+
+    ; terminate the handler
+    xor rax, rax
+
+    mov rsp, rbp
+    pop rbp
+
+    ret
 
 send_callback: ; (HttpRequest*, int fd)
     push rbp
