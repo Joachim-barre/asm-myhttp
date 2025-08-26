5efab85c7ecdba9cc95a02de49b90dc85eb56ae9
Joachim-barre <joachim.barre22@laposte.net>
2025-08-26 15:24:48 +0200

    added js code for events

--- exemples/simple_chat/static/index.js
+++ exemples/simple_chat/static/index.js
@@ -11,3 +11,14 @@ document.getElementById("send_form").addEventListener("submit", function (event)
         console.error("Error:", error);
     });
 });
+
+const evtSource = new EventSource("/events");
+const messageList = document.getElementById("messages");
+
+evtSource.onmessage = (e) => {
+    const newElement = document.createElement("li");
+
+    newElement.textContent = `${e.data}`;
+    newElement.classList.add('message_recived');
+    messageList.appendChild(newElement);
+};
