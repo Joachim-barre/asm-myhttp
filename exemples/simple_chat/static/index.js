f1e84dd8376d2cf772616b573bc0a6c7304e7069
Joachim-barre <joachim.barre22@laposte.net>
2025-08-27 19:28:27 +0200

    updated the javascript to support auto scrolling

--- exemples/simple_chat/static/index.js
+++ exemples/simple_chat/static/index.js
@@ -1,19 +1,34 @@
 let lastSent = ""
 const messageList = document.getElementById("messages");
 
+function appendMessage(message, is_sent) {
+    if (is_sent){
+        lastSent = message
+    } else if (lastSent == e.data) {
+        lastSent = ""
+        return
+    }
+
+    const newElement = document.createElement("li");
+
+    newElement.textContent = `${message}`;
+    if (is_sent) {
+        newElement.classList.add('message_sent');
+    } else {
+        newElement.classList.add('message_recived');
+    }
+    messageList.appendChild(newElement);
+
+    messageList.scrollTop=messageList.scrollHeight;
+}
+
 document.getElementById("send_form").addEventListener("submit", function (event) {
     event.preventDefault();
 
     var formData = new FormData(this);
     var message = formData.get("to_send")
 
-    lastSent = message
-
-    const newElement = document.createElement("li");
-
-    newElement.textContent = `${message}`;
-    newElement.classList.add('message_sent');
-    messageList.appendChild(newElement);
+    appendMessage(message, true)
 
     fetch("/send", {
         method: "POST",
@@ -27,16 +42,7 @@ document.getElementById("send_form").addEventListener("submit", function (event)
 const evtSource = new EventSource("/events");
 
 evtSource.onmessage = (e) => {
-    if (lastSent == e.data) {
-        lastSent = ""
-        return
-    }
-
-    const newElement = document.createElement("li");
-
-    newElement.textContent = `${e.data}`;
-    newElement.classList.add('message_recived');
-    messageList.appendChild(newElement);
+    appendMessage(e.data, false)    
 };
 
 evtSource.addEventListener("connected", (e) => {
